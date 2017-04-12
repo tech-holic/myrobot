fc137094cd481627fce6648c6a20fe58274a717f
Jingjing <940999723@qq.com>
2017-04-12 10:56:14 +0800

    Initial commit

---
+++ src/myrobot.java
@@ -0,0 +1,26 @@
+/**
+ * Created by techolic on 2017/4/11.
+ */
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+public class myrobot {
+    public static void main(String[] args) {
+        try {
+            Robot robot = new Robot();
+//定义5秒的延迟以便你打开notepad
+// Robot 开始写
+            robot.delay(5000);
+            robot.keyPress(KeyEvent.VK_H);
+            robot.keyPress(KeyEvent.VK_I);
+            robot.keyPress(KeyEvent.VK_SPACE);
+            robot.keyPress(KeyEvent.VK_C);
+            robot.keyPress(KeyEvent.VK_A);
+            robot.keyPress(KeyEvent.VK_O);
+            robot.keyPress(KeyEvent.VK_E);
+            robot.keyPress(KeyEvent.VK_R);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+}
